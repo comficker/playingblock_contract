29a7cd53e9be700f8c7adeb6328350ec157c14eb
Lock3r888 <110678920+Lock3r888@users.noreply.github.com>
2022-08-05 23:02:10 +0700

    feat: Roulette

--- contracts/Roulette.sol
+++ contracts/Roulette.sol
@@ -2,34 +2,34 @@
 pragma solidity ^0.8.0;
 
 contract Roulette {
-    event Bet(address indexed _player, uint _value, uint _ticket);
-    event Flipping(uint256 _round, uint _result);
-
-    struct Ticket {
-        bool withdraw;
-        uint amount_h;
-        uint amount_t;
-    }
-
-    struct Round {
-        uint256 start_time;
-        uint256 ticket_price;
-        bool complete;
-        mapping(address => Ticket) tickets;
-        uint result;
-        uint fee;
-        uint amount_h;
-        uint amount_t;
-    }
 
     address private _owner;
-    uint256 public _pool_balance;
-    uint256 public _ticket_price;
-    uint256 public _current_round;
-    uint public _pool_fee = 10;
-    Round[] private rounds;
-    bool _is_betting;
-    mapping(address => uint256[]) public _joined_rounds;
+
+    enum RouletteResult{ AVAX, BTC, SOL }
+
+    struct RoundInfo {
+        bool isFinished;
+        RouletteResult result;
+        uint256 totalBetted;
+        mapping(RouletteResult => uint256) totalBettedByResult;
+        mapping(address => mapping (RouletteResult => uint256)) betByAddress;
+        uint256 rewarded;
+    }
+
+    mapping (address => mapping (uint256 => bool)) public isClaimed;
+
+    mapping(uint256 => Round) public rounds;
+
+    uint256 public currentRoundId;
+
+    uint256 public totalDeposited;
+    uint256 public rewardPool;
+    mapping(address => uint256) depositByAddress;
+
+    uint256 public poolFee;
+    uint256 public totalFee;
+
+    uint256 public constant ONE_HUNDRED = 100;
 
     modifier onlyOwner() {
         require(_owner == msg.sender, "Ownable: caller is not the owner");
@@ -38,175 +38,88 @@ contract Roulette {
 
     constructor() {
         _owner = msg.sender;
-        _is_betting = true;
-        _ticket_price = 10000000 gwei;
-        _current_round = 0;
-        rounds.push();
-        Round storage round = rounds[_current_round];
-        round.start_time = block.timestamp;
-        round.fee = _pool_fee;
-        round.ticket_price = _ticket_price;
+        poolFee = 10;
     }
 
     function setTicketPrice(uint256 price) public onlyOwner {
         _ticket_price = price;
     }
 
+    function bet(RouletteResult betOption) payable external {
+        RoundInfo storage currentRound = rounds[currentRoundId];
+        currentRound.betByAddress[msg.sender][betOption] += msg.value;
+        currentRound.totalBettedByResult[betOption] += msg.value;
+        currentRound.totalBetted += msg.value;
+    }
+
+    function deposit() payable external {
+        totalDeposited += msg.value;
+        rewardPool += msg.value;
+        depositByAddress[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+        uint256 userDeposited = depositByAddress[msg.sender];
+        require(userDeposited, "You did NOT deposited!");
+        depositByAddress[msg.sender] = 0;
+        uint256 withdrawAmount = userDeposited * rewardPool / totalDeposited;
+        totalDeposited -= withdrawAmount;
+        rewardPool -= withdrawAmount;
+        uint256 feeing = withdrawAmount / ONE_HUNDRED;
+        totalFee += feeing;
+        withdrawAmount -= feeing;
+        (bool sent, ) = address(msg.sender).call{value: withdrawAmount}("");
+        require(sent, "Sendout reward failed!");
+        
+        emit Withdrawed(msg.sender, withdrawAmount)
+    }
+
+    function roll() onlyOwner external {
+        RoundInfo storage currentRound = rounds[currentRoundId];
+        uint256 lastRound = currentRoundId;
+        currentRoundId += 1;
+        currentRound.isFinished = true;
+        currentRound.result = RouletteResult(_random());
+        rewardPool += currentRound.totalBetted;
+        if (currentRound.result == RouletteResult.BTC) {
+            currentRound.rewarded = currentRound.totalBettedByResult[RouletteResult.BTC] * 10;
+        } else {
+            currentRound.rewarded = currentRound.totalBettedByResult[currentRound.result] * 2;
+        }
+        if (currentRound.rewarded >= rewardPool) {
+            currentRound.rewarded = rewardPool;
+        }
+        rewardPool -= currentRound.rewarded;
+        uint256 feeing = currentRound.rewarded * poolFee / ONE_HUNDRED;
+        currentRound.rewarded -= feeing;
+        totalFee += feeing;
+        
+        emit Rolled(lastRound, currentRound.result, currentRound.rewarded, feeing);
+    }
+
+    function claimReward(uint256 roundId) external {
+        RoundInfo storage thisRound = rounds[roundId];
+        require(!isClaimed[msg.sender][roundId], "You already claimed!");
+        uint256 userBetted = thisRound.betByAddress[msg.sender][thisRound.result];
+        require(userBetted > 0, "Not Bet on this result ;))");
+        uint256 claiming = userBetted * thisRound.rewarded / thisRound.totalBettedByResult;
+        (bool sent, ) = address(msg.sender).call{value: claiming}("");
+        require(sent, "Sendout reward failed!");
+        
+        emit Claimed(roundId, msg.sender, claiming);
+    }
+
     function setFee(uint fee) public onlyOwner {
-        require(fee < 100, "Feed: invalid");
-        _pool_fee = fee;
+        require(fee < 100, "Setting Fee: invalid fee");
+        poolFee = fee;
     }
 
-    function random() private view returns (uint){
-        return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % 2;
-    }
-
-    function bet(uint number, uint ticket) public payable {
-        Round storage round = rounds[_current_round];
-        require(round.start_time + 15 minutes > block.timestamp, "Round: timeout");
-        require(ticket * _ticket_price == msg.value, "Round: not enought money");
-        uint amount = ticket;
-        if (round.tickets[msg.sender].amount_t == 0 && round.tickets[msg.sender].amount_h == 0) {
-            _joined_rounds[msg.sender].push(_current_round);
-        }
-        if (number == 0) {
-            round.amount_t = round.amount_t + amount;
-            round.tickets[msg.sender].amount_t = round.tickets[msg.sender].amount_t + amount;
-        } else {
-            round.amount_h = round.amount_h + amount;
-            round.tickets[msg.sender].amount_h = round.tickets[msg.sender].amount_h + amount;
-        }
-
-        _pool_balance = _pool_balance + msg.value;
-        emit Bet(msg.sender, number, ticket);
-    }
-
-    function flip() public onlyOwner {
-        // DAO
-        // get result
-        uint rs = random();
-        Round storage round = rounds[_current_round];
-        round.result = rs;
-        round.complete = true;
-        _current_round = _current_round + 1;
-        //  create new round
-        rounds.push();
-        Round storage round2 = rounds[_current_round];
-        round2.start_time = block.timestamp + 1 minutes;
-        round2.fee = _pool_fee;
-        round2.ticket_price = _ticket_price;
-        emit Flipping(_current_round - 1, rs);
-    }
-
-    function getBooty(uint round_index, address wallet) public view returns(uint256) {
-        Round storage round = rounds[round_index];
-        if (round.tickets[wallet].withdraw) {
-            return 0;
-        }
-        uint256 balance = 0;
-        if (round.result == 0) {
-            if (round.amount_t > 0) {
-                balance = balance + _ticket_price * round.tickets[wallet].amount_t;
-                balance = balance + _ticket_price * round.tickets[wallet].amount_t * round.amount_h / round.amount_t;
-            }
-        } else {
-            if (round.amount_h > 0) {
-                balance = balance + _ticket_price * round.tickets[wallet].amount_h;
-                balance = balance + _ticket_price * round.tickets[wallet].amount_h * round.amount_t / round.amount_h;
-            }
-        }
-        return balance - balance * round.fee / 100;
-    }
-
-    function getBooties(address wallet) public view returns(uint256 balance) {
-        uint256[] storage joined_rounds = _joined_rounds[wallet];
-        for (uint256 i = 0; i < joined_rounds.length; i++) {
-            Round storage round = rounds[joined_rounds[i]];
-            uint256 round_balance = 0;
-            if (!round.tickets[wallet].withdraw) {
-                if (round.result == 0) {
-                    if (round.amount_t > 0) {
-                        round_balance = round_balance + _ticket_price * round.tickets[wallet].amount_t;
-                        round_balance = round_balance + _ticket_price * round.tickets[wallet].amount_t * round.amount_h / round.amount_t;
-                    }
-                } else {
-                    if (round.amount_h > 0) {
-                        round_balance = round_balance + _ticket_price * round.tickets[wallet].amount_h;
-                        round_balance = round_balance + _ticket_price * round.tickets[wallet].amount_h * round.amount_t / round.amount_h;
-                    }
-                }
-                round_balance = round_balance - round_balance * round.fee / 100;
-            }
-            balance = balance + round_balance;
-        }
-        return balance;
-    }
-
-    function claimBooty(uint round_index) public payable {
-        Round storage round = rounds[round_index];
-        Ticket storage ticket = round.tickets[msg.sender];
-        require(!ticket.withdraw, "Ticket: withdrawn");
-        uint256 balance = 0;
-        if (round.result == 0) {
-            if (round.amount_t > 0) {
-                balance = balance + _ticket_price * ticket.amount_t;
-                balance = balance + _ticket_price * ticket.amount_t * round.amount_h / round.amount_t;
-            }
-        } else {
-            if (round.amount_h > 0) {
-                balance = balance + _ticket_price * ticket.amount_h;
-                balance = balance + _ticket_price * ticket.amount_h * round.amount_t / round.amount_h;
-            }
-        }
-        balance = balance - balance * round.fee / 100;
-        require(balance > 0, "Ticket: booty is zero");
-        require(_pool_balance >= balance , "Pool: not enought");
-        uint256[] storage joined_rounds = _joined_rounds[msg.sender];
-        for (uint256 i = 0; i < joined_rounds.length; i++) {
-            if (joined_rounds[i] == round_index) {
-                joined_rounds[i] = joined_rounds[joined_rounds.length - 1];
-                joined_rounds.pop();
-                break;
-            }
-        }
-        _pool_balance = _pool_balance - balance;
-        payable(msg.sender).transfer(balance);
-        ticket.withdraw = true;
-    }
-
-    function claimBooties() public payable {
-        uint256[] storage joined_rounds = _joined_rounds[msg.sender];
-        uint256 total = 0;
-        for (uint256 i = 0; i < joined_rounds.length; i++) {
-            Round storage round = rounds[joined_rounds[i]];
-            Ticket storage ticket = round.tickets[msg.sender];
-            if (!ticket.withdraw) {
-                uint256 balance = 0;
-                if (round.result == 0) {
-                    if (round.amount_t > 0) {
-                        balance = balance + _ticket_price * ticket.amount_t;
-                        balance = balance + _ticket_price * ticket.amount_t * round.amount_h / round.amount_t;
-                    }
-                } else {
-                    if (round.amount_h > 0) {
-                        balance = balance + _ticket_price * ticket.amount_h;
-                        balance = balance + _ticket_price * ticket.amount_h * round.amount_t / round.amount_h;
-                    }
-                }
-                ticket.withdraw = true;
-                total = total + balance - balance * round.fee / 100;
-            }
-
-        }
-        require(total > 0, "Ticket: booty is zero");
-        require(_pool_balance >= total , "Pool: not enought");
-        payable(msg.sender).transfer(total);
-        _pool_balance = _pool_balance - total;
-        delete _joined_rounds[msg.sender];
+    function _random() private view returns (uint){
+        return uint(keccak256(abi.encodePacked(gasLeft(),block.timestamp, block.difficulty, msg.sender))) % 3;
     }
 
     function getRoundInfo(uint round_index) public view returns(uint, uint256, uint, uint256, uint, uint) {
-        Round storage round = rounds[round_index];
+        Round memory round = rounds[round_index];
         return (round_index, round.start_time, round.result, round.ticket_price, round.amount_h, round.amount_t);
     }
 
@@ -214,5 +127,25 @@ contract Roulette {
         require(_pool_balance >= amount, "Balance: not enought");
         payable(msg.sender).transfer(amount);
         _pool_balance = _pool_balance - amount;
+    }
+
+    struct Call {
+        address target;
+        bytes callData;
+    }
+
+    struct Result {
+        bool success;
+        bytes returnData;
+    }
+
+    function aggregate(Call[] memory calls) public onlyOwner returns (uint256 blockNumber, bytes[] memory returnData) {
+        blockNumber = block.number;
+        returnData = new bytes[](calls.length);
+        for(uint256 i = 0; i < calls.length; i++) {
+            (bool success, bytes memory ret) = calls[i].target.call(calls[i].callData);
+            require(success, "Multicall aggregate: call failed");
+            returnData[i] = ret;
+        }
     }
 }
